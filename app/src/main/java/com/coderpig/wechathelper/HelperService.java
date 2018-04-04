051e52943d6b8c8e48e31ab6957e78736f905d2f
CoderPig <779878443@qq.com>
2018-04-04 14:12:11 +0800

    Create Project

---
+++ app/src/main/java/com/coderpig/wechathelper/HelperService.java
@@ -0,0 +1,22 @@
+package com.coderpig.wechathelper;
+
+import android.accessibilityservice.AccessibilityService;
+import android.view.accessibility.AccessibilityEvent;
+
+/**
+ * 描述：
+ *
+ * @author CoderPig on 2018/04/04 13:46.
+ */
+
+public class HelperService extends AccessibilityService{
+    @Override
+    public void onAccessibilityEvent(AccessibilityEvent event) {
+
+    }
+
+    @Override
+    public void onInterrupt() {
+
+    }
+}
